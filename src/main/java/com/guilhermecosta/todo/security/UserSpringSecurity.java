9f7c938a64aae905a1a8a92f8bf86e87152bc925
Guilherme Costa <guilhermeldcosta@gmail.com>
2023-04-30 17:20:26 -0300

    Create UserSpringSecurity

---
+++ src/main/java/com/guilhermecosta/todo/security/UserSpringSecurity.java
@@ -0,0 +1,54 @@
+package com.guilhermecosta.todo.security;
+
+import com.guilhermecosta.todo.models.enums.ProfileEnum;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
+@Getter
+public class UserSpringSecurity implements UserDetails {
+
+    //Criar atributos iguais do usuario
+    private Long id;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserSpringSecurity(Long id, String username, String password, Set<ProfileEnum> profileEnums) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = profileEnums.stream().map(ref -> new SimpleGrantedAuthority(ref.getDescription())).collect(Collectors.toSet());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public boolean hasHole(ProfileEnum profileEnum) {
+        return getAuthorities().contains(new SimpleGrantedAuthority(profileEnum.getDescription()));
+    }
+}
